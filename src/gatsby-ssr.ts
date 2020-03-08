24826f566e48bc38d4f110a6f685e29c10f76339
Kim Jeker <git@kije.ch>
2020-03-08 13:53:02 +0100

    implemented SSR

--- src/gatsby-ssr.ts
+++ src/gatsby-ssr.ts
@@ -1,7 +1,33 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+import React from 'react';
+import { GatsbySSR, RenderBodyArgs } from 'gatsby';
+import { RESPONSE_PUBLIC_PATH } from './common';
+import { readFileSync, pathExistsSync } from 'fs-extra';
+import htmlParse from 'html-react-parser';
+import { GenerateFaviconResult } from 'rfg-api';
+import { RealFaviconPluginOptions } from './gatsby-node';
 
-// You can delete this file if you're not using it
+export const onRenderBody: NonNullable<GatsbySSR['onRenderBody']> = (
+  { setHeadComponents, reporter }: RenderBodyArgs,
+  options: RealFaviconPluginOptions,
+): any => {
+  if (!pathExistsSync(RESPONSE_PUBLIC_PATH)) {
+    reporter.warn(
+      '[gatsby-plugin-realfavicongenerator] response from realfavicongenerator.net does not exist. Skipping adding meta-tags during SSR!',
+    );
+    return;
+  }
+  const { favicon }: GenerateFaviconResult = JSON.parse(
+    readFileSync(RESPONSE_PUBLIC_PATH).toString(),
+  );
+
+  if (!favicon) {
+    reporter.warn(
+      '[gatsby-plugin-realfavicongenerator] could not read favicon html from realfavicongenerator.net response. Skipping adding meta-tags during SSR!',
+    );
+    return;
+  }
+
+  const components = htmlParse(favicon.html_code.replace('\n', '').trim());
+
+  setHeadComponents(components as React.ReactNode[]);
+};
